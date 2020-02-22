b8acdcbfe3c05a1d979f0c24261023eda7ca5fd2
Polluk <61083999+Polluk@users.noreply.github.com>
2020-02-23 00:01:59 +0100

    Update mipslabwork.c

--- mipslabwork.c
+++ mipslabwork.c
@@ -2,11 +2,7 @@
 
    This file written 2015 by F Lundevall
    Updated 2017-04-21 by F Lundevall
-
-   This file should be changed by YOU! So you must
-   add comment(s) here with your name(s) and date(s):
-
-   This file modified 2017-04-31 by Ture Teknolog
+   This file modified 2020 by Lukas Pöldma
 
    For copyright and licensing, see file COPYING */
 
@@ -15,22 +11,13 @@
 #include "mipslab.h"  /* Declatations for these labs */
 int mytime = 0x5957;
 
-volatile int* trisE = (volatile int*) 0xbf886100; //pekare port E I/O kontroll
-volatile int* portE = (volatile int*) 0xbf886110; //pekare port E läge
 int timeoutcount = 0; // ska öka när timer 2 når overflow
-
-int prime = 1234567;
 
 char textstring[] = "text, more text, and even more text!";
 
 /* Interrupt Service Routine */
 user_isr( void )
 {
-  if((IFS(0) >> 19) & 0x1){
-    *portE += 0x1;  // Ökar minnet som LED läser med 1
-    IFSCLR(0) = 0x80000;
-  }
-
   if((IFS(0) >> 8) & 0x1){  // kollar interrupt-flaggan
     timeoutcount++; // ökar timerräknaren
     IFSCLR(0) = 0x100; //återställer flaggan
@@ -51,10 +38,6 @@ void labinit( void )
 {
   *trisE &= ~0xff;  // Gör LED redo för output
   *portE = 0x0;     // Ser till att alla LED är av
-  TRISDSET = 0x800;   // gör switch 4 redo för input
-  IFSCLR(0) = 0x80000;
-  IECSET(0) = 0x80000;
-  IPCSET(4) = 0x1f;
 
   T2CON = 0x0;      // nollställer kontrollfunktioner för timer 2
   T2CONSET = 0x70;  // väljer skala 1:256
@@ -71,7 +54,5 @@ void labinit( void )
 /* This function is called repetitively from the main program */
 void labwork( void )
 {
-  prime = nextprime( prime );
-  display_string( 0, itoaconv( prime ) );
-  display_update( );
+
 }
